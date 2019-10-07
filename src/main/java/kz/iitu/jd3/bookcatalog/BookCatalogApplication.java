4763d4d41f655cd5bff587a2ac149193794642d8
Zhanbolat Seitkulov <zhanbolatseitkulov@gmail.com>
2019-10-07 18:39:38 +0600

    Adding timeout to RestTemplate

--- src/main/java/kz/iitu/jd3/bookcatalog/BookCatalogApplication.java
+++ src/main/java/kz/iitu/jd3/bookcatalog/BookCatalogApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -16,7 +17,9 @@ public class BookCatalogApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setConnectTimeout(3000);
+		return new RestTemplate(requestFactory);
 	}
 
 	public static void main(String[] args) {
